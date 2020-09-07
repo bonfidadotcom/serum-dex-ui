028fde599a2aca15614ee170cb1f5260fbaae70c
Bonfida <contact@bonfida.com>
2020-09-07 09:01:41 +0800

    Add files via upload

---
+++ precache-manifest.e43a8bcdc3472b211d3db3a17d025920.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eca83eb91778d0d5f447eba59370c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb6dcea89d9f26de26a4",
+    "url": "/static/css/2.872a6bf7.chunk.css"
+  },
+  {
+    "revision": "e94308796fee3e27eafe",
+    "url": "/static/css/main.57364c8a.chunk.css"
+  },
+  {
+    "revision": "eb6dcea89d9f26de26a4",
+    "url": "/static/js/2.8414da3f.chunk.js"
+  },
+  {
+    "revision": "55b1ec825146b1d590ddd190ffd831c9",
+    "url": "/static/js/2.8414da3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94308796fee3e27eafe",
+    "url": "/static/js/main.182d6ade.chunk.js"
+  },
+  {
+    "revision": "d3ebb56a3098223b5316",
+    "url": "/static/js/runtime-main.c7319a82.js"
+  },
+  {
+    "revision": "6678f7290b85a0a43737d642a6c5df2b",
+    "url": "/static/media/logo-big.6678f729.svg"
+  }
+]);
